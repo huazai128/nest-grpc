bdbce15e4dd6d9efb700eba3ab8a1d020434ecd0
zhouweihua <zhouweihua@joyy.com>
2024-06-20 14:00:51 +0800

    feat: 格式化文件

--- server/src/processors/database/database.providers.ts
+++ server/src/processors/database/database.providers.ts
@@ -1,37 +1,37 @@
-import { connection, disconnect, connect, set } from 'mongoose'
+import { connection, disconnect, connect, set } from 'mongoose';
 
-export const DB_CONNECTION_TOKEN = 'DB_CONNECTION_TOKEN'
-const  dbUrl = 'mongodb://127.0.0.1:27017/server'
+export const DB_CONNECTION_TOKEN = 'DB_CONNECTION_TOKEN';
+const dbUrl = 'mongodb://127.0.0.1:27017/server';
 
 export const databaseProviders = [
   {
     provide: DB_CONNECTION_TOKEN,
     useFactory: async () => {
-      let reconnectionTask: any = null
-      const RECONNECT_INTERVAL = 6000
-      set('strictQuery', true)
+      let reconnectionTask: any = null;
+      const RECONNECT_INTERVAL = 6000;
+      set('strictQuery', true);
 
       function dbConnect() {
-        return connect(dbUrl)
+        return connect(dbUrl);
       }
 
       connection.on('disconnected', () => {
-        reconnectionTask = setTimeout(dbConnect, RECONNECT_INTERVAL)
-      })
+        reconnectionTask = setTimeout(dbConnect, RECONNECT_INTERVAL);
+      });
 
       connection.on('open', () => {
-        console.info('mongodb数据库连接成功')
-        clearTimeout(reconnectionTask)
-        reconnectionTask = null
-      })
+        console.info('mongodb数据库连接成功');
+        clearTimeout(reconnectionTask);
+        reconnectionTask = null;
+      });
 
       connection.on('error', (error) => {
-        console.error('数据库连接异常', error)
-        disconnect()
-      })
+        console.error('数据库连接异常', error);
+        disconnect();
+      });
 
-      return dbConnect()
+      return dbConnect();
     },
     inject: [],
   },
-]
+];
