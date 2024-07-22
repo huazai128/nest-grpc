f8b03bd6d0b65480879d77079faea60b630ef437
zhouweihua <zhouweihua@joyy.com>
2024-07-22 17:25:51 +0800

    feat: 测试不同gRPC 方法🚀

--- server/modules/protousers/protousers.controller.ts
+++ server/modules/protousers/protousers.controller.ts
@@ -2,7 +2,8 @@ import { OrderService } from '@app/protos/orders/service'
 import { UserService } from '@app/protos/user'
 import { Controller, OnModuleInit, Inject, Get } from '@nestjs/common'
 import { ClientGrpc } from '@nestjs/microservices'
-import { lastValueFrom } from 'rxjs'
+import { lastValueFrom, ReplaySubject, Subject, toArray } from 'rxjs'
+import * as GRPC from '@grpc/grpc-js'
 
 /**
  * 用于验证和测试
@@ -28,6 +29,44 @@ export class ProtousersController implements OnModuleInit {
   getList() {
     return this.userService.find({
       id: 1,
+      itemTypes: [1],
     })
+  }
+  @Get('test1')
+  steamVerfiy() {
+    // ReplaySubject 是 RxJS 中的一个特殊类型的 Subject，它可以记录并重放一定数量的值给新的订阅者
+    // 这段代码的目的是将使用 ReplaySubject 记录的值传递给 userService.sync() 方法进行同步操作，并将同步结果作为 Observable 进行返回。
+    const ids$ = new ReplaySubject<any>()
+    ids$.next({ id: 1 })
+    ids$.next({ id: 4 })
+    ids$.complete()
+    //
+    const stream = this.userService.sync(ids$.asObservable())
+    // 将流中的所有值收集到一个数组中
+    return stream.pipe(toArray())
+  }
+
+  @Get('test2')
+  streamReq() {
+    const upstream = new ReplaySubject<any>()
+    upstream.next({
+      id: 1,
+      itemTypes: [1],
+    })
+    upstream.complete()
+    return this.userService.streamReq(upstream)
+  }
+
+  @Get('test3')
+  streamReqCall() {
+    const upstream = new Subject<any>()
+    upstream.next({
+      id: 1,
+      itemTypes: [1],
+    })
+    upstream.complete()
+
+    const call$ = this.userService.streamReqCall(upstream)
+    return call$
   }
 }
--- server/modules/protousers/protousers.module.ts
+++ server/modules/protousers/protousers.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { ClientsModule, Transport } from '@nestjs/microservices'
-import { join } from 'path'
+import { ReflectionService } from '@grpc/reflection'
 import { ProtousersController } from './protousers.controller'
+import { join } from 'path'
 
 @Module({
   imports: [
@@ -16,6 +17,10 @@ import { ProtousersController } from './protousers.controller'
           loader: {
             includeDirs: [join(__dirname, '../../protos')],
             keepCase: true,
+          },
+          onLoadPackageDefinition: (pkg, server) => {
+            // ReflectionService的作用是向gRPC客户端提供有关gRPC服务的元数据信息。它允许客户端查询和发现可用的gRPC服务，以及服务中的方法、消息类型和其他元数据。ReflectionService提供的元数据信息可以用于自动生成客户端代码、动态调用gRPC服务方法以及进行服务发现和探测等。
+            new ReflectionService(pkg).addToServer(server)
           },
           // protoPath: join(__dirname, '../../protos/user.proto'),
         },
