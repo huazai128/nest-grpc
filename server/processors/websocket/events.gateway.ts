d4a1dba82e5fc6fa89b0c9657e3e525f17f71893
zhouweihua <zhouweihua@joyy.com>
2024-07-26 18:32:17 +0800

    feat: websocket

--- server/processors/websocket/events.gateway.ts
+++ server/processors/websocket/events.gateway.ts
@@ -20,19 +20,32 @@ const Logger = createLogger({ scope: 'EventsGateway', time: isDevEnv })
   },
 })
 export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  handleDisconnect(client: any) {
-    Logger.log('连接关闭')
-  }
-
-  handleConnection(client: any, ...args: any[]) {
-    Logger.log('连接成功')
-  }
   @WebSocketServer()
   server: Server
 
+  handleDisconnect(client: any) {
+    Logger.log('websocket 连接关闭')
+  }
+
+  handleConnection(client: any, ...args: any[]) {
+    Logger.log('websocket 连接成功')
+  }
+
+  /**
+   * events 事件名称
+   * @param {*} data
+   * @return {*}  {Observable<WsResponse<number>>}
+   * @memberof EventsGateway
+   */
   @SubscribeMessage('events')
   handleMessage(@MessageBody() data: any): Observable<WsResponse<number>> {
     Logger.log('接收消息events的数据', data)
+    return from([1, 2, 3]).pipe(map((item) => ({ event: 'events', data: item })))
+  }
+
+  @SubscribeMessage('userLogin')
+  handleUserLogin(@MessageBody() data: any): Observable<WsResponse<number>> {
+    Logger.log('接收消息userLogin的数据', data)
     return from([1, 2, 3]).pipe(map((item) => ({ event: 'events', data: item })))
   }
 }
--- src/stores/SocketStore/index.ts
+++ src/stores/SocketStore/index.ts
@@ -9,8 +9,8 @@ export class SocketStore {
 
   @action
   initScoket = () => {
-    this.socket = io('ws://localhost:8081')
-    // this.socket = io('ws://172.26.130.15:8081')
+    // this.socket = io('ws://localhost:8081')
+    this.socket = io('ws://172.26.130.15:8081')
 
     // 监听连接成功
     this.socket.on('connect', () => {
@@ -36,11 +36,22 @@ export class SocketStore {
     })
   }
 
+  /**
+   * 向 event 发送 数据
+   * @param {string} event
+   * @param {*} data
+   * @memberof SocketStore
+   */
   @action
   emitWs = (event: string, data: any) => {
-    this.socket.emit('events', { data: 'q23123' })
+    this.socket.emit(event, data)
   }
 
+  /**
+   * 监听事件 进行处理
+   * @param {string} event
+   * @memberof SocketStore
+   */
   @action
   onMessage = <T>(event: string) => {
     return new Promise<T>((resolve, reject) => {
