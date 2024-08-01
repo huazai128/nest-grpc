bef98e06b9d89fedca31655702590b033b4aa32f
zhouweihua <zhouweihua@joyy.com>
2024-08-01 12:37:54 +0800

    feat: 监听pm2 脚本

--- scripts/pm2-monitor.sh
+++ scripts/pm2-monitor.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
-
+TO_USER="@all"  # 发送给所有人，也可以指定具体的用户
 # 获取所有与 PM2 相关的进程信息
 pm2_processes=$(ps -ef | grep pm2)
 
 message="PM2 Processes Status:\n"
-
-echo 
+# echo "PM2 process $pm2_processes is not running as expected. Please check!" 
 
 # 解析进程信息并检查状态
 while IFS= read -r line; do
-    if [[ $line == *"[P]M2"* ]]; then
+    if [[ $line == *"bin/pm2"* ]]; then
         # 提取进程名称
-        process_name=$(echo $line | awk '{print $8}')
+        process_name=$(echo $line | awk '{print $2}')
+
+        echo "$process_name"
         
         # 获取进程状态
         pm2_status=$(pm2 status $process_name)
-        
+
+        echo "$pm2_status"
+
+        specific_pm2_status=$(echo "$pm2_status" | grep -o "errored")
+
+        echo "$specific_pm2_status"
+
         # 判断进程状态是否正常
-        if [[ $pm2_status != *"online"* ]]; then
+        if [[ -z $specific_pm2_status ]]; then
             # 发送警报，可以使用邮件或其他通知方式
-            echo "PM2 process $process_name is not running as expected. Please check!" | mail -s "PM2 Process Alert" your_email@example.com
+            curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=af31299c-75f2-4c02-b67f-5796734bb901' \
+              -H 'Content-Type: application/json' \
+              -d '
+              {
+                    "msgtype": "text",
+                    "text": {
+                        "content": "pm2启动异常"
+                    }
+              }'
         fi
     fi
 done <<< "$pm2_processes"
+
