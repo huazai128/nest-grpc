2ec04592e4132937761796bd750bb6289f4bae59
zhouweihua <zhouweihua@joyy.com>
2024-08-12 22:34:05 +0800

    feat: 上报SDK开发

--- src/monitor/sendLog.ts
+++ src/monitor/sendLog.ts
@@ -2,6 +2,8 @@ import Config from './config'
 import { ConfigProps } from './interfaces/config.interface'
 import { MetricsName } from './interfaces/util.interface'
 import LogStore from './logStore'
+import 'reflect-metadata'
+import { Injectable } from '@tanbo/di'
 
 /**
  * 发送日志
@@ -9,6 +11,7 @@ import LogStore from './logStore'
  * @class SendLog
  * @extends {LogStore}
  */
+@Injectable()
 export class SendLog extends LogStore {
   public readonly config!: ConfigProps
   constructor() {
@@ -17,6 +20,13 @@ export class SendLog extends LogStore {
   }
 
   handlerCommon(key: MetricsName | string): void {
-    console.log('=====')
+    if (!this.keys.includes(key)) {
+      this.keys.push(key)
+    }
+    if (!this.isOver) {
+      this.isOver = true
+      this.handleRoutineReport()
+    }
   }
+  private handleRoutineReport() {}
 }
